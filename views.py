4cc2231141bdc8dffa457be1f5262b44ebcc5afc
gulfairus-aliyar <80202702+gulfairus-aliyar@users.noreply.github.com>
2022-02-19 12:10:43 +0600

    Update views.py

--- views.py
+++ views.py
@@ -1,23 +1,57 @@
-from django.shortcuts import render, redirect
-from . models import Product
-from . forms import ProductForm
+from django.shortcuts import render
+import requests
+from urllib.request import urlopen
+import json
+from scan.models import Accounts
 
-# Create your views here.
+ethereum_token = 'D2R66WU1RBHPPDIGVET25491BTQBNAA97J'
+
 
 def index(request):
-    products = Product.objects.all()
+    accounts = Accounts.objects.all()
+    list_acc = []
 
-    if request.method == 'POST':
-        form = ProductForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('index')
-    else:
-        form = ProductForm()        
+
+    for i in range(5):
+        address = ""
+        length = 0
+
+        if i == 0:
+            length = 20
+        elif i == 1:
+            length = 40
+        elif i == 2:
+            length = 60
+        elif i == 3:
+            length = 80
+        else:
+            length = 100
+
+        for j in range(length-20, length):
+            if j == 19:
+                address += accounts[j].address
+            else:
+                address += accounts[j].address + ","
+        coin_api = 'https://api.etherscan.io/api?module=account&action=balancemulti&address=' + address + '&tag=latest&apikey=' + ethereum_token
+
+
+
+        result = requests.get(url=coin_api)
+        for i in (result.json())['result']:
+            list_acc.append(i)
+
+
+    for i in list_acc:
+        acc = Accounts.objects.get(address=i['account'])
+        acc.balance = i['balance']
+        print(acc)
+        acc.save()
+
+
 
     context = {
-        "products": products,
-        "form": form
+        "data" : list_acc
     }
 
-    return render(request, 'chartapp/index.html', context)
+
+    return render(request, 'chartapp/index.html', {"list_acc": list_acc} )
